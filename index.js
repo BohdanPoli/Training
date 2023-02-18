30636ff3933106aa1177acb2b9f128362c7bb0d1
Bohdan Polishchuk <polishuk.bog@gmail.com>
2023-02-18 18:55:29 +0200

    add class

--- index.js
+++ index.js
@@ -1,1 +1,65 @@
-console.log(Math.abs(-100));
+class User {
+  static sayHello() {
+    console.log("Hi");
+  }
+
+  sayGoodBye() {
+    console.log("Bye");
+  }
+
+  constructor({ email = "standard", password = "standard" } = {}) {
+    this._email = email;
+    this.password = password;
+  }
+
+  get email() {
+    return this._email;
+  }
+
+  set email(newEmail) {
+    this._email = newEmail;
+  }
+}
+
+const newUser = new User({ email: "123@gmail", password: "12312" });
+
+class SuperUser extends User {
+  constructor({ nick, email, password } = {}) {
+    super({ email, password });
+
+    this._nick = nick;
+  }
+}
+
+const superI = new SuperUser({
+  email: "LALA@@@SUPER",
+  password: "Without",
+  nick: "Ball",
+});
+
+class SuperPix extends SuperUser {
+  constructor({ slogan, ...restProps } = {}) {
+    super(restProps);
+
+    this._slogan = slogan;
+  }
+
+  get slogan() {
+    return this._slogan;
+  }
+}
+
+const superIo = new SuperPix({
+  slogan: "play all time",
+  email: "super@123",
+  password: "12312",
+  nick: "Dad",
+});
+
+console.log(newUser);
+console.log(superI);
+console.log(superIo);
+
+console.log(superIo.email);
+
+console.log(parseInt("I1"));
